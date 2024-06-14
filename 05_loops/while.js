e131dda003715f4bf9513357eeb9572503b35716
Omkar Nilawar <omkarnilawar9@gmail.com>
2024-06-15 00:18:38 +0530

    while loop

---
+++ 05_loops/while.js
@@ -0,0 +1,17 @@
+//while loop :- while loop is used to execute a block of code as long as the condition is true.
+//syntax :- 
+/* 
+    while (consition)
+    {
+        codeblock
+    }
+    the code block will only execute until the condition is true.
+*/
+
+//ex -
+let i = 0;
+while(i < 10)
+{
+    console.log(i);
+    i++; //increment the i value so it does not go into infinite loop.
+}
