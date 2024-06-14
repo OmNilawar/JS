7248cfb787efb2e2339451aa130b701bdd056aa1
Omkar Nilawar <omkarnilawar9@gmail.com>
2024-06-15 01:50:35 +0530

    do while loop

---
+++ 05_loops/Do-While.js
@@ -0,0 +1,23 @@
+//Do while Loop :-
+
+//The do while loop is similar to the while loop except that the condition is always executed after the statements inside the loop are executed. This means that the statements inside the loop are always executed at least once.
+
+/* 
+Syntax :-
+do
+{
+    code block
+}
+    while(condition);
+*/
+
+//the diffrence is here the code will execute atleast once 
+
+//ex -
+let i = 11;
+do
+{
+    console.log(i);
+    i++;
+}
+while(i < 10); //it will execute atleast once.
